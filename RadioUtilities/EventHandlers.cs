a2fcd045911d2aa0e2ecc36f68c164fd205c0092
borat <borat@REDOX>
2023-09-22 16:56:49 +0300

    Proje dosyası ekle.

---
+++ RadioUtilities/EventHandlers.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.Events.EventArgs.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioUtilities
+{
+    internal class EventHandlers
+    {
+        public void OnUsingRadioBattery(UsingRadioBatteryEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+    
+    
+    }
+}
---
+++ RadioUtilities/MainPlugin.cs
@@ -0,0 +1,29 @@
+﻿
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Exiled.Events;
+
+namespace RadioUtilities
+{
+    public class MainPlugin : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            EventHandlers eventHandlers = new EventHandlers();
+
+            Exiled.Events.Handlers.Player.UsingRadioBattery += eventHandlers.OnUsingRadioBattery;
+        }
+        public override void OnDisabled()
+        {
+            EventHandlers eventHandlers = new EventHandlers();
+
+            Exiled.Events.Handlers.Player.UsingRadioBattery -= eventHandlers.OnUsingRadioBattery;
+        }
+
+    }
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+}   
